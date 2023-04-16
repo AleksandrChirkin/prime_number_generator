15a4e7220fde1e222b4b834df11c8c2dcf2c4dc6
Alexander Chirkin <chirkin2001@list.ru>
2023-04-16 17:24:14 +0500

    adding backups

--- app.sh
+++ app.sh
@@ -1,7 +1,9 @@
-rm generator/migrations/*_initial.py
-rm db.sqlite3*
-python3 manage.py migrate
-python3 manage.py makemigrations generator
-python3 manage.py sqlmigrate generator 0001
-python3 manage.py migrate
+if [ ! -f "backup_params.txt" ]; then
+  rm generator/migrations/*_initial.py
+  rm db.sqlite3*
+  python3 manage.py migrate
+  python3 manage.py makemigrations generator
+  python3 manage.py sqlmigrate generator 0001
+  python3 manage.py migrate
+fi
 python3 manage.py runserver 8000
--- generator/generator.py
+++ generator/generator.py
@@ -1,11 +1,19 @@
 from django.core.exceptions import AppRegistryNotReady
 from django.db.utils import IntegrityError
+from pathlib import Path
 
 import logging
 import math
+import signal
 
 
 class Generator:
+    def __init__(self):
+        self.backup = 'backup_params.txt'
+        self.q = 1
+        self.R = 2
+        signal.signal(signal.SIGINT, self.write_backup)
+
     def generate(self):
         while True:
             try:
@@ -13,29 +21,45 @@ class Generator:
                 break
             except AppRegistryNotReady:
                 continue
-        q = 1
-        # добавляем "2" в БД, чтобы потом можно было с ее помощью генерировать недостающие простые числа
-        Certificate.objects.get_or_create(N='2', a=2, q='1')
-        cert_id = 1
+        recovered = False
+        if Path(self.backup).exists():
+            with open(self.backup) as backup_file:
+                backup_content = backup_file.read().split(',')
+            self.q = int(backup_content[0])
+            self.R = int(backup_content[1])
+            cert_id = Certificate.objects.get(N=self.q).id
+            recovered = True
+        else:
+            # добавляем "2" в БД, чтобы потом можно было с ее помощью генерировать недостающие простые числа
+            Certificate.objects.get_or_create(N='2', a=2, q='1')
+            cert_id = 1
         while True:
-            R = 2
-            while R < 4 * (q + 1):
-                N = q * R + 1
+            if recovered:
+                recovered = False
+            else:
+                self.R = 2
+            while self.R < 4 * (self.q + 1):
+                N = self.q * self.R + 1
                 first_decree = N - 1
-                second_decree = int(first_decree / q)
+                second_decree = int(first_decree / self.q)
                 if pow(2, first_decree, N) == 1 and (first_decree % second_decree == 0 or
                                                      pow(2, second_decree, N) != 1):
                     try:
-                        Certificate.objects.get_or_create(N=N, a=2, q=q)
-                        logging.info(f'({N}, 2, {q})')
+                        Certificate.objects.get_or_create(N=N, a=2, q=self.q)
+                        logging.info(f'({N}, 2, {self.q})')
                     except IntegrityError:
                         pass
                 elif self.brute_force_check(N):
                     logging.info(f'{N} is prime but was not certified by 2!')
-                R += 2
+                self.R += 2
             # получить следующее простое число из БД
-            q = int(Certificate.objects.get(id=cert_id).N)
+            self.q = int(Certificate.objects.get(id=cert_id).N)
             cert_id += 1
+
+    def write_backup(self, signum, frame):
+        with open(self.backup, mode='w') as backup_params:
+            backup_params.write(f'{self.q},{self.R}')
+        exit()
 
     @staticmethod
     def brute_force_check(N: int) -> bool:
