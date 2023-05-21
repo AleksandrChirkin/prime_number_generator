91cc5ba76ce4839ef56c3a6c148e8aaab571b1e0
Alexander Chirkin <chirkin2001@list.ru>
2023-05-21 10:36:30 +0500

    устранил проблему с непосредственным запуском на Linux

--- app.sh
+++ app.sh
@@ -1,12 +1,12 @@
 sudo apt install python3-django
 if [ ! -f "secret_key.txt" ]; then
-  ./generate_secret.py
+  python3 generate_secret.py
 fi
 if [ ! -f "db.sqlite3" ]; then
   rm generator/migrations/*_initial.py
-  ./manage.py migrate
-  ./manage.py makemigrations generator
-  ./manage.py sqlmigrate generator 0001
-  ./manage.py migrate
+  python3 manage.py migrate
+  python3 manage.py makemigrations generator
+  python3 manage.py sqlmigrate generator 0001
+  python3 manage.py migrate
 fi
-./manage.py runserver 8000
+python3 manage.py runserver 8000
